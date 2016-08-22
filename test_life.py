6cbc7d219b497c6e9b6f5197f01b3d777be4a8f7
Labeeb <labeebg2@gmail.com>
2016-08-22 23:31:25 +0530

    test for is_alive() fails RED

--- test_life.py
+++ test_life.py
@@ -6,3 +6,11 @@ life_grid"""
     ip = [[0,0,0], [0,1,1], [1,0,0]]
     grid.create_grid(ip)
     assert grid.life_grid == ip
+
+
+def test_is_alive ():
+    """ This tests, for the given position if the cell is alive True if alive, false otherwise. """
+
+    ip = [[0,0,0], [0,1,1], [1,0,0]]
+    assert grid.is_alive(1, 1) == True
+
