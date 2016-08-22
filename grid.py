d1f612624888e29066d50a045837b54dc975b15b
Labeeb <labeebg2@gmail.com>
2016-08-22 23:48:05 +0530

    Test passes for is_alive() GREEN

--- grid.py
+++ grid.py
@@ -3,3 +3,9 @@ life_grid = None
 def create_grid(ip):
     global life_grid
     life_grid = ip
+
+def is_alive(x,y):
+    if life_grid[x][y] == 1:
+        return True
+    else:
+        return False
