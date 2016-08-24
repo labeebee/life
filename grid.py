4c0f841dec9c89049ee74bad037eace56da97e1d
Labeeb <labeebg2@gmail.com>
2016-08-24 16:32:36 +0530

    refactors to the current stage. However birth_cell() doens't work in apply_rules() call.

--- grid.py
+++ grid.py
@@ -1,53 +1,58 @@
-life_grid = None
+class Grid:
+    life_grid = None
 
-def create_grid(ip):
-    global life_grid
-    life_grid = ip
+    def create_grid(self, ip):
+        global life_grid
+        self.life_grid = ip
 
-def is_alive(x,y):
-    if life_grid[x][y] == 1:
-        return True
-    else:
-        return False
-def num_neighbours(x,y):
-    ncount = 0
-    for xn in [x-1, x, x+1]:
-        for yn in [y-1, y, y+1]:
-            if xn < 0 or yn < 0 or xn >= len(life_grid) or yn >= len(life_grid):
-                pass
-            elif xn == x and yn == y:
-                pass
-            elif (is_alive(xn,yn) == True):
-                ncount += 1
-    return ncount
+    def is_alive(self, x, y):
+        global life_grid
+        if self.life_grid[x][y] == 1:
+            return True
+        else:
+            return False
+    def num_neighbours(self, x, y):
+        ncount = 0
+        for xn in [x-1, x, x+1]:
+            for yn in [y-1, y, y+1]:
+                if xn < 0 or yn < 0 or xn >= len(self.life_grid) or yn >= len(self.life_grid):
+                    pass
+                elif xn == x and yn == y:
+                    pass
+                elif (self.is_alive(xn,yn) == True):
+                    ncount += 1
+        return ncount
 
-def kill_cell(x, y):
-    life_grid[x][y] = 0
+    def kill_cell(self, x, y):
+        self.life_grid[x][y] = 0
 
-def birth_cell(x, y):
-    life_grid[x][y] = 1
+    def birth_cell(self, x, y):
+        self.life_grid[x][y] = 1
 
-def size():
-    return len(life_grid)
+    def size(self):
+        return len(self.life_grid)
 
-def grid_as_array():
-    return life_grid
+    def grid_as_array(self):
+        return self.life_grid
 
 #Layer 1
-
-def apply_rules():
-    for x in range(0, size()):
-        for y in range(0, size()):
-            if is_alive(x, y) and num_neighbours(x, y) < 2:
-                kill_cell(x, y)
-            elif is_alive(x, y) and num_neighbours(x, y) == (2, 3):
+def apply_rules(A):
+    B = Grid()
+    B.create_grid(A.life_grid)
+    for x in range(0, A.size()):
+        for y in range(0, A.size()):
+            if A.is_alive(x, y) and A.num_neighbours(x, y) < 2:
+                B.kill_cell(x, y)
+                break
+            elif A.is_alive(x, y) and A.num_neighbours(x, y) == (2, 3):
                 pass
-            elif is_alive(x, y) and num_neighbours(x, y) > 3:
-                kill_cell(x, y)
-            elif is_alive(x, y) and num_neighbours == 3:
-                birth_cell(x, y)
-
-
-
+            elif A.is_alive(x, y) and A.num_neighbours(x, y) > 3:
+                B.kill_cell(x, y)
+                break
+            elif not A.is_alive(x, y) and A.num_neighbours(x, y) == 3:
+                B.birth_cell(x, y)
+                break
+    A.life_grid = B.life_grid
+    return A.life_grid
 
 
--- test_life.py
+++ test_life.py
@@ -1,80 +1,66 @@
 import grid
 
-#Layer 0
-
+A = grid.Grid()
+ip = None
 def test_create_grid():
-    """Makes sure that the input is stored inside the grid module as 
-life_grid"""
+    global ip
     ip = [[0,0,0], [0,1,1], [1,0,0]]
-    grid.create_grid(ip)
-    assert grid.life_grid == ip
+    A.create_grid(ip)
+    assert A.life_grid == ip
 
+def test_is_alive():
+    assert A.is_alive(1, 1) == True
+    assert A.is_alive(0, 0) == False
+    assert A.is_alive(2, 1) == False
 
-def test_is_alive ():
-    """ This tests, for the given position if the cell is alive True if alive, false otherwise. """
-
-    ip = [[0,0,0], [0,1,1], [1,0,0]]
-    assert grid.is_alive(1, 1) == True
-    assert grid.is_alive(0, 0) == False
 
 def test_num_neighbours():
-    """This tests the number of neighbours function for a given position"""
-    ip = [[0,0,0], [0,1,1], [1,0,0]]
     """for the cell in the middle"""
-    assert grid.num_neighbours(1, 1) == 2
+    assert A.num_neighbours(1, 1) == 2
     """for the left edge"""
-    assert grid.num_neighbours(1, 0) == 2
+    assert A.num_neighbours(1, 0) == 2
     """for the right edge"""
-    assert grid.num_neighbours(1, 2) == 1
+    assert A.num_neighbours(1, 2) == 1
     """for the top edge"""
-    assert grid.num_neighbours(0, 1) == 2
+    assert A.num_neighbours(0, 1) == 2
     """for the bottom edge"""
-    assert grid.num_neighbours(2, 1) == 3
+    assert A.num_neighbours(2, 1) == 3
     """for the top left corner"""
-    assert grid.num_neighbours(0, 0) == 1
+    assert A.num_neighbours(0, 0) == 1
     """for the top right corner"""
-    assert grid.num_neighbours(0, 2) == 2
+    assert A.num_neighbours(0, 2) == 2
     """for the bottom left corner"""
-    assert grid.num_neighbours(2, 0) == 1
+    assert A.num_neighbours(2, 0) == 1
     """for the bottom right corner"""
-    assert grid.num_neighbours(2, 2) == 2
-    
-    
-    
-def test_kill_cell():
-    """tests if the function kills a cell at a given position"""
-    ip = [[0,0,0], [0,1,1], [1,0,0]]
-    grid.kill_cell(1, 1)
-    assert grid.is_alive(1, 1) == False
-    grid.kill_cell(2, 0)
-    assert grid.is_alive(2, 0) == False
+    assert A.num_neighbours(2, 2) == 2
 
+def test_kill_cell():
+    A.kill_cell(1, 1)
+    assert A.is_alive(1, 1) == False
+    A.kill_cell(2, 0)
+    assert A.is_alive(2, 0) == False
 
 def test_birth_cell():
-    """tests if the function gives birth to a live cell at a given position"""
-    ip = [[0,0,0], [0,1,1], [1,0,0]]
-    grid.birth_cell(0, 0)
-    assert grid.is_alive(0, 0) == True
+    A.birth_cell(0, 0)
+    assert A.is_alive(0, 0) == True
+    A.kill_cell(0, 0)
+    A.birth_cell(0, 2)
+    assert A.is_alive(0, 2) == True
+    A.kill_cell(0, 2)
+    
     
 def test_size():
-    """tests if the fuction returns the size of the grid without any input"""
-    assert grid.size() == 3
+    assert A.size() == 3
 
 def test_grid_as_array():
-    """tests if the fuction returns a grid of the same dimension as list of lists"""
-    ip = [[0,0,0], [0,1,1], [1,0,0]]
-    assert len(grid.grid_as_array()) == len(ip)
+    assert len(A.grid_as_array()) == len(ip)
     
-    
-#Layer 1
-
 def test_apply_rules():
-    ip = [[0,0,0], [0,1,1], [1,0,0]]
-    print (ip)
-    print (grid.life_grid)
-    grid.apply_rules()
-    print (grid.life_grid)
-    assert grid.is_alive(2, 1) == True
-    assert grid.is_alive(1, 2) == False
+    print (A.life_grid[2][1])
+    print (A.life_grid[1][2])  
+    grid.apply_rules(A)
+    print (A.life_grid[2][1])
+    print (A.life_grid[1][2])
+    assert A.is_alive(2, 1) == True
+    assert A.is_alive(1, 2) == False
     
-        
